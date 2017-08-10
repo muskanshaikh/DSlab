210d675e66dbfeef1ad3f844ecec00d090ce6a7f
muskanshaikh <muskanshaikh2798@gmail.com>
2017-08-10 22:53:01 +0530

    Update inpost.c

--- inpost.c
+++ inpost.c
@@ -28,53 +28,50 @@ int main()
 	return 0;
 
 }
-
 void convert(char in[],char post[])
 {
 	char opr;
 	stack s1;
 	int j=0,i;
 	s1.top=-1;
-	for(i=0;in[i]!=0;i++)
+	for(i=0;in[i]!='\0';i++)
 	{
-		if(isalpha(in[j])
-		{
-		post[j++]=in[i];
-		if(in[1]=='(')
-		push(&s1,in[i]);
-		if(in[1]=='='||in[i]=='/'||in[i]=='*')
+		if(isalpha(in[i]))
+			post[j++]=in[i];
+		if(in[i]=='(')
+			push(&s1,in[i]);
+		if(in[i]=='+'||in[i]=='-'||in[i]=='/'||in[i]=='*')
 		{
 			if(s1.top!=-1)
 			{
 				opr=pop(&s1);
-				while(priority(opr)>=priority(in[1]))
+				while(priority(opr)>=priority(in[i]))
 				{
-					post[j++]=opr;
-					opr=pop(&s1);
+					 post[j++]=opr;
+					 opr=pop(&s1);
 				}
 				push(&s1,opr);
-				push(&s1,in[1]);
-			}
-			else
-			{
 				push(&s1,in[i]);
 			}
-			if(in[i]==')')
+			else
+				push(&s1,in[i]);
+		}
+		if(in[i]==')')
+		{
+			opr=pop(&s1);
+			while(opr!='(')
 			{
+				post[j++]=opr;
 				opr=pop(&s1);
-				while(opr!='(')
-				{
-					post[j++]=opr;
-					opr=pop(&s1);
-				}
 			}
 		}
-		while(s1.top!=-1)
-		post[j++]=pop(&s1);
-		post[j]='\0';
 	}
-	int priority(char c)
-	{
+	while(s1.top!=-1)
+		post[j++]=pop(&s1);
+	post[j]='\0';
+ }
+int priority(char c)
+{
 		if(c=='$')
 		return 3;
 		if(c=='/'||c=='*')
@@ -83,14 +80,14 @@ void convert(char in[],char post[])
 		return 1;
 		else 
 		return 0;
-	}
-	void push(stack *s,char opr)
-	{
+}
+void push(stack *s,char opr)
+{
 		s->top++;
-		s->a[s->[top]]=opr;
-	}
-	char pop(stack *s)
-	{
+		s->a[s->top]=opr;
+}
+char pop(stack *s)
+{
 		if(s->top==-1)
 	{
 		printf("stack overflow");
